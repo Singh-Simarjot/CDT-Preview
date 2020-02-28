513ef6ed897eb4305014bbada8eab46c9c9b4f18
harwinder-mp <harwinder.singh@millipixels.com>
2020-02-28 11:21:19 +0530

    codeSnippets changes

--- src/preview/components/codeSnippets/codeSnippets.jsx
+++ src/preview/components/codeSnippets/codeSnippets.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
+// import Editor from "react-simple-code-editor";
+// import { highlight, languages } from "prismjs/components/prism-core";
 import "./codeSnippets.scss";
 class CodeSnippets extends Component {
   state = {};
   render() {
     const { data } = this.props;
     return (
-      <div className="codeSnippets"  id ={data.id}>
+      <div className="codeSnippets" id={data.id}>
         <h1>{data.title}</h1>
         <p>{data.description}</p>
-        <Editor
+        <div className="dataCode">
+          <pre>{data.content.code}</pre>
+        </div>
+        {/* <Editor
           value={data.content.code}
           highlight={code => highlight(code, languages.js)}
           padding={10}
@@ -19,7 +22,7 @@ class CodeSnippets extends Component {
             fontFamily: '"Fira code", "Fira Mono", monospace',
             fontSize: 12
           }}
-        />
+        /> */}
       </div>
     );
   }
